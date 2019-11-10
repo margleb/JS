031ca03853f834f2caf03fb25beba8cf275347e0
Gleb Martianov <margleb@yandex.ru>
2019-11-10 23:28:27 +0300

    Variables and Data Types из курса по JS

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('it works')
+/*
+1. Переменные не могут начинатся с цифр, за исключением $ или _
+2. Нельзя использовать зарезервированные слова в качестве переменных (например, function, delete, if)
+*/
+
+/* В javascript 5 различных типов данных */
+
+// bool
+var fullAge = true;
+console.log(fullAge);
+
+// undefined
+var job;
+console.log(job);
+
+// string
+var lastname = 'Smith';
+console.log(job);
+
+// number
+var age = 28;
+console.log(job);
+
+// null (не существует)
