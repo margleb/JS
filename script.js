6f50a43c52fa63e7676b419b6383493fcc96aabc
Gleb Martianov <margleb@yandex.ru>
2019-11-11 07:14:23 +0300

    The Ternary Operator and Switch Statements из курса по JS

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 /**************************
-* Boolean logic
-  AND(&&)
-  OR(||)
-  NOT(!)
+* The Ternary Operator and Switch Statements
 */
 
 var firstName = 'John';
-var age = 20;
+var age = 16;
 
-if(age < 13) {
-    console.log(firstName + ' is a boy.');
-} else if(age >= 13 && age < 20) { // Between 13 and 20
-    console.log(firstName + ' is a teenager.');       
-} else if(age >= 20 && age < 30) {
-     console.log(firstName + ' is a young man');
+// Ternary operator
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+/* if(age >= 18) {
+    var drink = 'beer';
 } else {
-    console.log(firstName + ' is a man.');
+    var drink = 'juice';
+} */
+
+// Switch statement
+var job = 'teacher';
+switch (job) {
+    case 'teacher':
+        console.log(firstName + ' teacher kids  how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beatiful websites');
+        break;
+    default:
+        console.log(firstName + ' does something else');
 }
