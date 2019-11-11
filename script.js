99820db358afeb6cdcfeae8a18cb31b64b94e951
Gleb Martianov <margleb@yandex.ru>
2019-11-11 07:27:56 +0300

    Objects and properties из курса по Laravel

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-function tipCalculator(bill) {
-    var percentage;
-    if(bill < 50) {
-        percentage = .2;
-    } else if(bill >= 50 && bill < 200) {
-        percentage = .2;
-    } else {
-        percentage = .1;
-    }
-    return percentage * bill;
-}
+/**********************
+* Objects and properties
+*/
 
-console.log(tipCalculator(10));
+// john object
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false
+};
+// Доступ до элемента обьекта
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+// console.log(john[x]);
 
+// mutate
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
 
-var bills = [124, 48, 268];
-var tips = [tipCalculator(bills[0]),
-            tipCalculator(bills[1]),
-            tipCalculator(bills[2])];
-
-var finalValues = [bills[0] + tips[0],
-                   bills[1] + tips[1],
-                   bills[2] + tips[2]];
-
-console.log(tips, finalValues);
+// создание нового пустого обьекта
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
