5f23b6f9d3ca4da52eab5ff79e73a01a0ee3f7ec
Gleb Martianov <margleb@yandex.ru>
2019-11-11 07:24:48 +0300

    Arrays из курса по JS

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 /******************
-* Functions Statements and Expresssions
+* Arrays
 */
 
-// Function declaration
-function whatDoYouDo(job, firstName) {}
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
 
-// Function expression
-var whatDoYouDo = function (job, firstName) {
-    switch(job) {
-        case 'teacher':
-            return firstName + ' teaches  kids how to code';
-        case 'driver':
-            return firstName + ' drives a cab in Lisbon';
-        case 'designer':
-            return firstName + ' designs beatiful websites';
-        default:
-            return firstName + ' does something else';
-    }
-}
+console.log(names[0]);
+console.log(names.length);
 
-console.log(whatDoYouDo('teacher', 'John'));
-console.log(whatDoYouDo('designer', 'Jane'));
-console.log(whatDoYouDo('retired', 'Mark'));
+// Mutate array date
+names[1] = 'Ben'
+// последний элемент в массиве
+names[names.length] = 'Mary';
+console.log(names);
+
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
+// добавляет элемент в конец массива
+john.push('blue');
+// добавляет элемент в начало массива
+john.unshift('Mr.');
+console.log(john);
+
+// удаляет элемент из начала массива
+john.pop();
+// удаляет элемент из конца массива
+john.shift()
+console.log(john);
+// возращает позицию элемента в массиве
+// если элемента нет в массиве, то верент -1
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
