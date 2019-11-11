d7efcd224bf93bd834bc1b6ce661c9da859b7a50
Gleb Martianov <margleb@yandex.ru>
2019-11-11 07:29:27 +0300

    Objects and Methods из курса по JS

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 /**********************
-* Objects and properties
+* Objects and methods
 */
 
 // john object
 var john = {
     firstName: 'John',
     lastName: 'Smith',
-    birthYear: 1990,
+    birthYear: 1992,
     family: ['Jane', 'Mark', 'Bob', 'Emily'],
     job: 'teacher',
-    isMarried: false
+    isMarried: false,
+    // метод
+    calcAge: function() {
+        // доступ до св-ва данного обьекта через this
+        // установка нового св-ва age
+        this.age =  2018 - this.birthYear;
+    }
 };
-// Доступ до элемента обьекта
-console.log(john.firstName);
-console.log(john['lastName']);
-var x = 'birthYear';
-// console.log(john[x]);
 
-// mutate
-john.job = 'designer';
-john['isMarried'] = true;
+// доступ до метода
+// console.log(john.calcAge(1990));
+
+// инициализируем метод calcAge(); 
+john.calcAge();
 console.log(john);
-
-// создание нового пустого обьекта
-var jane = new Object();
-jane.firstName = 'Jane';
-jane.birthYear = 1969;
-jane['lastName'] = 'Smith';
-console.log(jane);
