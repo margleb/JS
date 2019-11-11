3fc418b75768c08f15f473ec67b67a523046f479
Gleb Martianov <margleb@yandex.ru>
2019-11-11 22:29:18 +0300

    Creating Objects Object.create из курса по JS

--- script.js
+++ script.js
@@ -1,53 +1,22 @@
-/* Function constructor 
- * Для проверки того, что обьект является прототипом функции-конструктора,
- * в консоле пишем john.__proto__ == Person.prototype
- *
- * Для проверки, является ли класс экзепляром функции-конструктора, в консоле пишем
- * john instanceof Person
- * 
- * В JS почти все является обьектом, для того чтобы в этом убедиться используем конструкцию 
- * console.info()
- */
+/* Object.create */
 
-// Обьект
-// var john = {
-//    name: 'John',
-//    yearOfBirth: 1990,
-//    job: 'teacher'
-// }
-
-// Функция-конструктор
-// Начинается с большой буквы
-var Person = function(name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-//    this.calculateAge = function() {
-//        console.log(2016 - this.yearOfBirth);
-//    }
-}
-
-// добавление метода к прототипу,
-// через св-во prototype
-Person.prototype.calculateAge = function() {
-    console.log(2016 - this.yearOfBirth);
+// обьект
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
 };
 
-// добавление св-ва к прототипу
-// через св-во prototype
-Person.prototype.lastName = 'Smith';
+// Создание обьекта john
+var john = Object.create(personProto);
+// Добавление свойств
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-// экземпляры классов
-var john = new Person('John', 1990, 'tacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
-
-// методы
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-
-// вызов lastName
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+// cоздание обьекта janе со свойствами
+var jane = Object.create(personProto, {
+   name: { value: 'Jane' },
+   yearOfBirth: { value: 1969 },
+   job: { value: 'designer' }
+});
