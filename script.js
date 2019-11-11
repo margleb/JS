4912589b23a738cf4b111e2cf181ce9e16bf132d
Gleb Martianov <margleb@yandex.ru>
2019-11-11 07:40:13 +0300

    Scoping and the Scope Chain из курса по JS

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 ///////////////////////////////////////
-// Lecture: Hoisting
+// Lecture: Scoping
 
-/* совершенно неважно где были объявлены функция или переменные, 
-все они передвигаются вверх своей области видимости, 
-вне зависимости от того локальная она или же глобальная. 
-Это называется механизм “поднятия” или в оригинальном названии hoisting */
 
-/*** Функции ***/
+// First scoping example
 
-// function declaration
-// calculateAge(1965);
-// function calculateAge(year) {
-    // console.log(2016 - year);
-// }
+/* scope chane */
+/* Функции имеют доступ до переменных, 
+вложенных функция и до глобальных переменных */
+//var a = 'Hello!';
+//first();
+//function first() {
+//    var b = 'Hi!';
+//    second();
+//    function second() {
+//        var c = 'Hey!';
+//        console.log(a + b + c);
+//    }
+//}
 
-// function expression
-/* механизм поднятия работает ТОЛЬКО с function declarations */
-// retirement(1990);
-// var retirement = function(year) {
-    // console.log(65 - (2016-year));
-// }
-
-/*** Переменные ***/
-
-/* механизм поднятия НЕ работает с age (годами) underfined */
-// console.log(age);
-// var age = 23;
-
-// global execution context;
-var one = 23;
-
-function foo() {
-    // undefined
-    console.log(one);
-    // local execution context;
-    var one = 65;
-    // выводит var 65;
-    console.log(one);
+/* execution stack */
+var a = 'Hello!';
+first();
+function first() {
+    var b = 'Hi!';
+    second();
+    function second() {
+        var c = 'Hey!';
+        third();
+    }
 }
 
-foo();
-// выводит var 23;
-console.log(one);
+function third() {
+    var d = 'John';
+    // функция не имеет доступа до переменной c возвращает underfined
+    console.log(c);
+    // имеет доступ до глобальной переменно a и локальной d
+    console.log(a + d);
+}
