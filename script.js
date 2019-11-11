c9fbc4097685f643820325277a2086568dc2bf67
Gleb Martianov <margleb@yandex.ru>
2019-11-11 22:30:50 +0300

    Primitives vs. Objects из курса по JS

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-/* Object.create */
+/* primitives vs objects */
 
-// обьект
-var personProto = {
-    calculateAge: function() {
-        console.log(2016 - this.yearOfBirth);
-    }
+/* Примитивы */
+// var a = 23;
+// var b = a;
+// a = 46;
+// сonsole.log(a); // 46
+// console.log(b); // 23
+
+
+/* Обьекты */
+// var obj1 = {
+    // name: 'John',
+    // age: 26
+// };
+
+// cоздается ссылка на обьект
+// var obj2 = obj1;
+// obj1.age = 30;
+// console.log(obj1.age); // 30
+// console.log(obj2.age); // 30
+
+
+// Функции
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon',
 };
 
-// Создание обьекта john
-var john = Object.create(personProto);
-// Добавление свойств
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'teacher';
+/* в качестве b
+передается не обьект 
+а ссылка на него */
+function change(a, b) {
+    a = 30;
+    b.city = 'Gleb';
+}
 
-// cоздание обьекта janе со свойствами
-var jane = Object.create(personProto, {
-   name: { value: 'Jane' },
-   yearOfBirth: { value: 1969 },
-   job: { value: 'designer' }
-});
+change(age, obj);
+
+// age остался неизменным
+console.log(age);
+// Lisbon изменился на San Francisco
+console.log(obj.city);
