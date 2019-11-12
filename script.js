2463ddc7db88c4a8df9b1b69a9802e4db9553f04
Gleb Martianov <margleb@yandex.ru>
2019-11-12 07:02:32 +0300

    Clousers из курса по JS

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 //////////////////////////
-// Lecture: IIFE
+// Lecture: Closures
 
-/* IIFE (Immediately Invoked Function Expression) это JavaScript функция, которая выполняется сразу же после того, как она была определена. */
-
-/*
-function game() {
-    var score = Math.random() * 10;
-    console.log(score >= 5); 
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
 }
-game();
-*/
 
-(function() {
-    var score = Math.random() * 10;
-    console.log(score >= 5); 
-})();
-// undefined
-// console.log(score);
+// первая функция
+var retirmentUS = retirement(66);
+var retirmentGermany = retirement(65);
+var retirmentIceland = retirement(67);
+// вторая анонимная функция
+retirmentGermany(1990);
+retirmentUS(1990);
+retirmentIceland(1990);
 
-// с передачей параметра
-(function(goodLuck) {
-    var score = Math.random() * 10;
-    console.log(score >= 5 - goodLuck); 
-})(5);
+// переменная a остается в памяти, 
+// даже после возвращения внутренней функции
+// retirement(66)(1990);
 
-
-
+// параметр job можно будет использовать,
+// даже псле того, как анониманя функция
+// будет возвращена
+function interviewQuestion(job) {
+    return function(name) {
+          if(job === 'designer') {
+            console.log(name + ', can you pls explain what UX desig');  
+          } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');  
+          } else {
+            console.log('Hello ' + name + ', what do you do?');  
+          }
+        }
+    }
+    
+// параметр job остается даже после возвращения
+// анонимной функции
+interviewQuestion('teacher')('John');
