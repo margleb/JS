d7db99b0029f7fa90f63fc79f2dca30cf1444262
Gleb Martianov <margleb@yandex.ru>
2019-11-12 06:57:09 +0300

    First Class Functions Functions Returning Functions из курса по JS

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
 //////////////////////////
-// Lecture: Passing functions as arguments
+// Lecture: Functions returning functions
 
-var years = [1990, 1965, 1937, 2005, 1998];
-
-// колбак функция
-function arrayCalc(arr, fn) {
-    var arrRes = []; 
-    for (var i = 0; i < arr.length; i++) {
-        // push - вставляет элементы в конец массива
-        // колбек функция
-        arrRes.push(fn(arr[i]));
-    }
-    return arrRes;
-}
-
-function calculateAge(el) {
-    return 2016 - el;
-}
-
-
-function isFullAge(el) {
-    return el >= 18;
-}
-
-// частота сердцебиения
-function maxHeartRate(el) {
-    if(el >= 18 && el <= 81) {
-     return Math.round(206.9 - (0.67 * el));   
+function interviewQuestion(job) {
+    if(job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you pls explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
     } else {
-        return -1;
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
     }
 }
 
-/* мы не вставляем () для 
-calculateAge, потому
-что хотим вызывть функцию
-внутири arrayCalc "колбек функция" */
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var retes = arrayCalc(ages, maxHeartRate);
+// сохраняем функцию в соответствии с работой
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+// возращаем текст вопроса
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
 
-console.log(ages);
-console.log(fullAges);
-console.log(retes);
+// вызывается функция фнути функции
+interviewQuestion('teacher')('Mark');
+
 
