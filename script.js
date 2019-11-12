268d3d30c54ebfd3a502a89cd4cc4e628c288bea
Gleb Martianov <margleb@yandex.ru>
2019-11-12 07:17:41 +0300

    Setting up the First Event Listeners из курса по JS

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-// модуль
+// Budget controller
 var budgetController = (function() {
-    // private
-    var x = 23;
-    var add = function(a) {
-        return x + a;
-    }  
-    /* Closures */
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-    
+    // Some code   
 })();
 
-// модуль
+// Ui controller
 var UIController = (function() {
     // Some code
 })();
 
-// связующий модуль для budgetController и UIController
+// Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
-    /* для лучшей практики, лучше всего передаваемые
-    параметры называть другими именами, например
-    budgetController = budgetCtrl; */
-    var z = budgetCtrl.publicTest(5);
     
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+       // 1. Get the field input data
+       // 2. Add the item to the budget controller
+       // 3. Add the item to the UI
+       // 4. Calculate the budget
+       // 5. Display the budget on the UI
+       console.log('It works');
     }
+    
+    // событие при клике на кнопку
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    // событие при нажатии на кнопку на клавиатуре
+    document.addEventListener('keypress', function(event) {
+        // witch для старых браузеров
+        // 13 - кнопка enter
+        if(event.keyCode === 13 || event.witch == 13) {
+            ctrlAddItem();
+        }
+    })
+    
 })(budgetController, UIController);
