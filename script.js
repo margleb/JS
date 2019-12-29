adca749a35560e921dbd538e2aa3a5dc05a141a1
Gleb Martianov <margleb@yandex.ru>
2019-12-29 10:29:20 +0300

    Formatting Our Budget Numbers: String Manipulation

--- script.js
+++ script.js
@@ -136,6 +136,20 @@ var UIController = (function() {
         container: '.container',
         expensesPercLabel: '.item__percentage'
     };
+    
+var formatNumber = function(num, type) {
+         var numSplit, int, dec, type;
+         num = Math.abs(num);
+         num = num.toFixed(2);
+         numSplit = num.split('.');
+         int = numSplit[0];
+         if(int.length > 3) {
+             int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length);
+         }
+         dec = numSplit[1];
+         return (type == 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+    };
+    
     return {
         // Метод, возвращающие 3 значения
         getInput: function() {
@@ -161,7 +175,7 @@ var UIController = (function() {
             // Replace the plactholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
@@ -185,9 +199,11 @@ var UIController = (function() {
             
         },       
         displayBudget: function(obj) {
-           document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget; 
-           document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc; 
-           document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp; 
+           var type;
+           obj.budget > 0 ? type = 'inc' : type = 'exp';  
+           document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type); 
+           document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc'); 
+           document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp'); 
            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
             
             if(obj.percentage > 0) {
