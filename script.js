a66950d58a66e769c3c9c311f72d5c6bdb041a33
Gleb Martianov <margleb@yandex.ru>
2020-01-02 22:04:20 +0300

    Rest Parameters

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 /////////
-// Lecture: Spread operator
+// Lecture: Rest parameters
 
-function addFourAges(a, b, c, d) {
-    return a + b + c + d;
+/*
+// ES5
+function isFullAge5() {
+    // console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+    argsArr.forEach(function(cur) {
+       console.log((2016 - cur) >= 18); 
+    });
 }
 
-var sum1 = addFourAges(18, 30, 12, 21);
-console.log(sum1);
+// isFullAge5(1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
 
-// ES5
-var ages = [18, 30, 12, 21];
-// apply функция подставляет массив как аргументы в функцию
-var sum2 = addFourAges.apply(null, ages);
-console.log(sum2);
 
 // ES6
-// spread operator
-const sum3 = addFourAges(...ages);
-console.log(sum3);
+// трансформирует параметры в массив и вставляет их в функцию
+function isFullAge6(...years) {
+    years.forEach(cur => console.log((2016 - cur) >= 18));
+}
+
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+*/
 
 
-const falulySmith = ['John', 'Jane', 'Mark'];
-const familyMiller = ['Mary', 'Bob', 'Ann'];
-const bigFamily = [...falulySmith, 'Lily', ...familyMiller];
-console.log(bigFamily);
+function isFullAge5(limit) {
+    // второй параметр call позволяет вычленить взять в массив параметры без учета первого
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+    argsArr.forEach(function(cur) {
+       console.log((2016 - cur) >= limit);
+    });
+}
+
+// isFullAge5(10, 1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
 
 
-const h = document.querySelector('h1');
-const boxes = document.querySelectorAll('.box');
-const all = [h, ...boxes];;
-Array.from(all).forEach(cur => cur.style.color = 'purple');
+// ES6
+// трансформирует параметры в массив и вставляет их в функцию
+function isFullAge6(limit, ...years) {
+    years.forEach(cur => console.log((2016 - cur) >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
