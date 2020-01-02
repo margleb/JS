de01b92ea5783227a6be443df9be446ec52ce192
Gleb Martianov <margleb@yandex.ru>
2020-01-02 11:07:36 +0300

    Destructuring

--- script.js
+++ script.js
@@ -1,76 +1,34 @@
-// Lecture Arrow function 2
+// Lecture: Destructuring
 
 // ES5
-var box5 = {
-    color: 'green',
-    position: 1,
-    clickMe: function() {
-        var self = this; // костыль для избегания проблемы c window обьектом
-        document.querySelector('.green').addEventListener('click', function() {
-           var str = 'This is box number ' + self.position + ' and it is ' + self.color;
-           alert(str);
-        });
-    }
-}
-
-// this указывает на window обьект, поэтому возвращает underfined
-// box5.clickMe();
+var john = ['John', 26];
+// var name = john[0];
+// var age = john[1];
 
 
 // ES6
-const box6 = {
-    color: 'green',
-    position: 1,
-    clickMe: function() {
-        // arrow function shares the lexical this keyword
-        document.querySelector('.green').addEventListener('click', () => {
-           var str = 'This is box number ' + this.position + ' and it is ' + this.color;
-           alert(str);
-        });
-    }
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
 }
 
-box6.clickMe();
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
 
-/*
-const box66 = {
-    color: 'green',
-    position: 1,
-    clickMe: () => { // теперь мы делимся глобальным this, который указывает на обьект window
-        document.querySelector('.green').addEventListener('click', () => {
-           var str = 'This is box number ' + this.position + ' and it is ' + this.color;
-           alert(str);
-        });
-    }
-}
-*/
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
 
-// здесь будет опять underfined, так как у window обьекта нет position и color
-// box6.clickMe();
-
-function Person(name) {
-    this.name = name;
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
 }
 
-// ES5
-Person.prototype.myFriends5 = function(friends) {
-    var arr = friends.map(function(el) {
-       // имя не передается, так как в данном случае this указывает на обьект window
-       return this.name + ' is friends with ' + el; 
-    }.bind(this)); // с помочью функции bind 'связывание', передаем this в анонимную функцию
-    console.log(arr);
-}
-
-var friends = ['Bob', 'Jane', 'Mark'];
-new Person('John').myFriends5(friends);
-
-
-// ES6
-Person.prototype.myFriends6 = function(friends) {
-    var arr = friends.map(el => 
-    // имя не передается, так как в данном случае this указывает на обьект window
-    `${this.name} is friends with ${el}`); 
-    console.log(arr);
-}
-
-new Person('Mike').myFriends6(friends);
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
