1eacc6a6eb8c753aecd9592bcc0457d8c62eebb2
Gleb Martianov <margleb@yandex.ru>
2020-01-02 21:39:09 +0300

    The Spread Operator

--- script.js
+++ script.js
@@ -1,50 +1,32 @@
 /////////
-// Lecture: Arrays
+// Lecture: Spread operator
+
+function addFourAges(a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5
+var ages = [18, 30, 12, 21];
+// apply функция подставляет массив как аргументы в функцию
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+// spread operator
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+
+const falulySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...falulySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+
+const h = document.querySelector('h1');
 const boxes = document.querySelectorAll('.box');
-
-// ES5
-/*
-var boxesArr5 = Array.prototype.slice.call(boxes);
-boxesArr5.forEach(function(cur) {
-   cur.style.backgroundColor = 'dodgerblue'; 
-});
-*/
-
-// ES6
-const boxesArr6 = Array.from(boxes);
-Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
-
-// ES5
-/*
-for(var i = 0; i < boxesArr5.length; i++) {
-    if(boxArr5[i].className === 'box blue') {
-        continue;
-    }
-    boxesArr5[i].textContent = 'I changed to blue!';
-}
-*/
-
-// ES6
-for (const cur of boxesArr6) {
-    if(cur.className.includes('blue')) {
-        continue;
-    }
-    cur.textContent = 'I changed to blue!';
-}
-
-
-var ages = [12, 17, 8, 21, 14 , 11];
-
-// ES5
-/*
-var full = ages.map(function(cur) {
-   return cur >= 18; 
-});
-console.log(full);
-console.log(full.indexOf(true));
-console.log(ages[full.indexOf(true)]);
-*/
-
-// ES6
-console.log(ages.findIndex(cur => cur >= 18));
-console.log(ages.find(cur => cur >= 18));
+const all = [h, ...boxes];;
+Array.from(all).forEach(cur => cur.style.color = 'purple');
