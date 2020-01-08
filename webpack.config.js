71426aefb88b5bf65c81d8591d48dcf2b4b3db10
Gleb Martianov <margleb@yandex.ru>
2020-01-08 15:34:21 +0300

    A Modern Setup: The Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
 /** 
-1. Cоздаем экспортируемый обьект для webpack с настройками (файл webpack.config.js)
-    - Cоздаем "входную точку" (entry:'./src/js/index.js') c указанием файла с которого webpack начне пакетирование зависимостей
-    - Создаем обьект output ("выходную  точку"), указывающую где сохранить скомпилированный файл
-        - переменная path c указанием абсолютного пути __dirname + папка сборки 'dist/js';
-        - имя фафла filename: 'bundle.js'
-    - Указываем мод разработки 'development' или для минификации файла 'production'
-2. Тестируем   
-    - Создаем файл test.js в src/js папке
-      - сonsole.log('Imported module');
-      - экспортируем дефолтное значение 23;
-    - В index.js импортируем num из './test';
-      - console.log('I impoerted ${num} from another module!');
-3. Импортируем npm скрипт в package.json
-4. Установиваем интерфейс командной строки npm webpack-cli --save-dev.
-5. Запускаем скрипт npm run dev через командрную строку
-6. Создаем index.html файл в  папке dist/index.html
-   - разметка html + подключаем bundgle.js
-   - проверяем что в консоле выводится console.log
-7. Cоздаем в package.json два скрипта dev и build
-   - dev: webpack --mode development, // для разработки
-   - build: webpack --mode production // минифицированный для продашена
+1. Устанавливаем npm i webpack-dev-server --save-dev (автоматическая сборка + reload браузера)
+   - Добавляем обьект devServer
+       - указываем contentBase: './dist' (где devServer должен мониторить наши файлы)
+       - contentBase и output path должы быть одинаковыми
+   - Добавляем start: "webpack-dev-server --mode devolepment --open" скрипт в  package.json
+     (флаг open указывает на автоматическе открытие окна браузера при запуске скрипта)
+
+2. Устанавливаем npm i html-webpack-plugin --save-dev позваляеющий автоматически перегенерировать файл index.html в dist 
+   (перегенерированный файл не сохраняется на диск! Для этого используем npm run dev)
+   - Подключаем плагин в webpack.config.js require('html-webpack-plugin');
+   - В обьект module.exports, добавляем массив plugins: [new HTMLWebpackPlugin()]
+     - В качестве аргумента передаем обьект cо свойствами:
+       - filename: 'index.html',
+       - template: './src/index.html'
 **/
 
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/bundle.js'
   },
-  // mode: 'development'
+  devServer: {
+    contentBase: './dist'
+},
+  plugins: [new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+  })],
 };
