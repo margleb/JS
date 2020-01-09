416a7c3595cbfaf319ac051eed868358ed743835
Gleb Martianov <margleb@yandex.ru>
2020-01-09 11:34:47 +0300

    A Modern Setup: Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,71 @@
 /** 
-1. Устанавливаем npm i webpack-dev-server --save-dev (автоматическая сборка + reload браузера)
-   - Добавляем обьект devServer
-       - указываем contentBase: './dist' (где devServer должен мониторить наши файлы)
-       - contentBase и output path должы быть одинаковыми
-   - Добавляем start: "webpack-dev-server --mode devolepment --open" скрипт в  package.json
-     (флаг open указывает на автоматическе открытие окна браузера при запуске скрипта)
+1. Устанавливаем пакеты:
+   - npm install babel-core (осн. функционал компилятора) babel-preset-env babel-loader --save-dev
+2. Устанавливаем обьект module:
+   module: {
+       rules: [
+           {
+               test: '/\.js$/',
+               exclude: '/node_modules/',
+               use: {
+                   loader: 'babel-loader'
+               }
+           }
+       ]
+   }
+3. Cоздаем конфиг файл для babel .babelrc
+   - cоздаем обьект
+      - { "presets": [
+        ["env", {
+            "targets": {
+                "browser": [
+                    "last 5 versions",
+                    "ie >= 8"
+                ]
+            }
+        }
+            
+        ]
+    ]
+}  
+4. Устанавливаем пакет babel-polyfill --save (работает с фичами которых нет в ES5)
+5. Добавляем, в entry 'babel-polyfill'
 
-2. Устанавливаем npm i html-webpack-plugin --save-dev позваляеющий автоматически перегенерировать файл index.html в dist 
-   (перегенерированный файл не сохраняется на диск! Для этого используем npm run dev)
-   - Подключаем плагин в webpack.config.js require('html-webpack-plugin');
-   - В обьект module.exports, добавляем массив plugins: [new HTMLWebpackPlugin()]
-     - В качестве аргумента передаем обьект cо свойствами:
-       - filename: 'index.html',
-       - template: './src/index.html'
+6. Тестируем
+   - в index.js добавляем еще одну переменную константу и выводим в консоль
+   - пробуем запустить npm run dev
 **/
 
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'js/bundle.js'
-  },
-  devServer: {
-    contentBase: './dist'
-},
-  plugins: [new htmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-  })],
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
