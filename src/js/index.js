43ca34e83f75aadbc6dab6719ba3ad138477afce
Gleb Martianov <margleb@yandex.ru>
2020-01-09 14:56:59 +0300

    Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,5 @@
 // Global app controller
+import Search from './model/search';
 
-/***
-1. Регистриуемся на сервисе
-   - API http://forkify-api.herokuapp.com/
-2. Устанавливаем axios --save и подключем его
-3. Cоздаем async функцию getRecepie()
-4. Создаем константы proxy/url/query/key
-5. Возращаем данные о рецептах в соответствии с запросом
-   - используем await
-6. Обрабатываем вывод ошибок try/catch
-***/
-
-var axios = require('axios');
-
-async function getRecepie(query) {
-    const url = `https://forkify-api.herokuapp.com/api/search?q=${query}`;
-    try {
-        let dataRec = await axios(url);
-        let Recepies = dataRec.data.recipes;
-        console.log(Recepies);   
-    } catch(error) {
-        alert(error);
-    }
-}
-
-getRecepie('pizza');
+var Recepie = new Search('pizza');
+Recepie.getRecepie();
---
+++ src/js/model/Search.js
@@ -0,0 +1,28 @@
+/***
+1. Cоздаем класс экспортируемый Search (не забываем сделать "экспорт по умолчанию")
+   - в construct() передаем параметр query
+   - переносим мето getRecipie из контроллера
+     - cохраняем полученные данные от сервера в переменную data
+   - import axios from 'axios'
+2. Импортируем Search модель в контроллер
+3. Пытаемся получить рецепты в контроллере
+***/
+
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    async getRecepie() {
+        const url = `https://forkify-api.herokuapp.com/api/search?q=${this.query}`;
+        try {
+            const dataRec = await axios(url);
+            this.result = dataRec.data.recipes;
+            console.log(this.result);
+        } catch(error) {
+            alert(error);
+        }
+    }   
+}
+
