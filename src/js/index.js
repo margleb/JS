83b24836cdcadfcd6e5b672b068d95eb4fc6299f
Gleb Martianov <margleb@yandex.ru>
2020-01-10 10:22:58 +0300

    Building the Search View - Part 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,5 @@
 // Global app controller
 
-/***
-1. Cоздаем файл base.js в папке Views
-   - экпортируем обьект элементов DOM
-2. Cоздаем функцию getInput() в SearchView.js, 
-   - получает значение (value) из поля поиска (ES6)
-3. Создаем функцию renderRecepie в SearchView.js,
-   - принимает конкретный рецепт (html разметку) и заносит его данные в html
-4. Создаем функцию renderResults() в SearchView.js,
-   - перебирает каждый рецепт по API
-5. Создаемочищающие функции в SearchView.js
-   - сlearInput() из getInput()  и clearResults() из DOM (innerHTML)
-***/
-
 import Search from './model/Search';
 import * as SearchView from './views/SearchView';
 import {DOMElements} from './views/base';
--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -1,3 +1,13 @@
+/****
+1. Cоздаем функцию лимитированного тайтла рецепта
+   - передаем аргументами title; limit=17
+   - условие, если заголовок больше чем limit
+   - функцией split() разбиваем строку и проходися функцией reduce()
+     - если текущая длина мньше лиминитрованой, то заносим слово в новый массив
+     - возращаем acc + cur.length
+   - функцией join() соединяем массив и возвращаем его как строку
+****/
+
 import {DOMElements} from './base'
 
 
@@ -6,6 +16,29 @@ export const getInput = () => DOMElements.inputField.value;
 
 // очищаем значение из getInput()
 export const clearInput = () => DOMElements.inputField.value = '';
+
+// функция лимитированного тайтла рецепта
+const titlelimit = (title, limit = 17) => {
+    var newArr = [];
+    if(title.length > limit) {
+        
+        title.split(' ').reduce((acc, cur) => {
+            
+            if(acc + cur.length <= limit) {
+               newArr.push(cur);
+            }
+            
+            // аккумулирует
+            return acc + cur.length;
+        }, 0);
+        
+        // возращает урезанную стрку
+        return `${newArr.join(' ')} ...`
+    }
+    
+    // по дефолту возращает title
+    return title;
+}
 
 // заносим конкретный рецепт в html
 const renderRecepie = recepie => {
@@ -16,7 +49,7 @@ const DOMelement =
          <img src="${recepie.image_url}" alt="${recepie.title}">
       </figure>
       <div class="results__data">
-         <h4 class="results__name">${recepie.title}</h4>
+         <h4 class="results__name">${titlelimit(recepie.title)}</h4>
          <p class="results__author">>${recepie.publisher}</p>
       </div>
    </a>
