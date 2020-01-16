4bd11950dc027261a6acc650ba43ff2a038a038c
Gleb Martianov <margleb@yandex.ru>
2020-01-16 15:28:06 +0300

    Wrapping up: Final Considerations

--- src/js/views/RecipeView.js
+++ src/js/views/RecipeView.js
@@ -20,14 +20,15 @@ export const highlightSelector = (id) => {
 
 const formatCount = (count) => {
     if(count) {
-        
-        const [int, dec] = count.toString().split('.').map(el => parseInt(el, 10));
-        if(!dec) return count;
+        // исправляем баг с большим кол-вом знаков после запятой
+        const newCount = Math.round(count * 10000) / 10000;
+        const [int, dec] = newCount.toString().split('.').map(el => parseInt(el, 10));
+        if(!dec) return newCount;
         if (int === 0) {
-            const fr = new Fraction(count);
+            const fr = new Fraction(newCount);
             return `${fr.numerator}/${fr.denominator}`
         } else {
-            let fr = new Fraction(count - int);
+            let fr = new Fraction(newCount - int);
             return `${int} ${fr.numerator}/${fr.denominator}`;
         }
         return '?';
